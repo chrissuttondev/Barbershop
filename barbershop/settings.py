8ece3e8bfd4732df3dc925809b44f68e2e825d82
Chris Sutton <chrissutton1789@gmail.com>
2024-02-24 17:36:29 +0000

    deployment settings

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-chrissutton-appointment-y0jmxkoks7d.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-chrissutton-appointment-oui4hj17ugo.ws-eu108.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
