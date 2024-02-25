07a7ee9bb2d28832f72077f65ac85dcf797742aa
Chris Sutton <chrissutton1789@gmail.com>
2024-02-25 19:07:08 +0000

    Added authentication

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("home.urls"), name="home-urls"),
+    path("accounts/", include("allauth.urls")),
     path("appointments/", include("appointments.urls"), name="appointments-urls"),
     path('admin/', admin.site.urls),
 ]
