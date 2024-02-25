60d8762c7e2b28cb7b20415b57a1c065148483af
Chris Sutton <chrissutton1789@gmail.com>
2024-02-25 21:01:39 +0000

    Added class for appointment booking form

---
+++ appointments/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import appointment_booking
+
+class AppointmentForm(forms.ModelForm):
+    """
+    Class for users to book appointemnts
+    """
+    class Meta:
+        """
+        Model and fields
+        """
+        model = appointment_booking
+        fields = ('name', 'service', 'date', 'time', 'notes')
--- appointments/views.py
+++ appointments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import AppointmentForm
 
 # Create your views here.
 def appointments(request):
@@ -7,4 +8,14 @@ def appointments(request):
         request,
         "appointments/appointments.html"
         )
+
+    if 
+
+    booking_form =BookingForm()
+    
+    return render(
+        request,
+        "appointments/appointments.html",
+        {'booking_form': booking_form}
+        )    
 
