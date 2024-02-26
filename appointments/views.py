1cf5b34aabab10a3d50606266754e535ba7a364c
Chris Sutton <chrissutton1789@gmail.com>
2024-02-26 11:40:55 +0000

    Issue: attempting to overide allauth sign redirect behaviour so that a user is redirected to the appointments page on succesfull sign in. Currently not woeking.

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,19 @@
+from allauth.account.views import SignupView
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
 from .forms import AppointmentForm
+
+# https://stackoverflow.com/questions/60404987/how-can-i-overide-django-allauth-signup-success-url
+
+# view to redirect user to appointments.html after sign up success
+
+# class signin_success(SignupView):
+def get_success_url(self):
+    return reverse_lazy('appointments') 
+
+
 
 # Create your views here.
 def appointments(request):
